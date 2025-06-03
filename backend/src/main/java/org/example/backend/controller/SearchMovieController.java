cc6bb299be886aff651e4f153dcfa5f7c9784611
Yinli <jiangyinli0803@gmail.com>
2025-06-03 15:02:33 +0200

    add search movie function--backend

---
+++ backend/src/main/java/org/example/backend/controller/SearchMovieController.java
@@ -0,0 +1,25 @@
+package org.example.backend.controller;
+
+import org.example.backend.exeption.MovieNotFoundException;
+import org.example.backend.model.Movie;
+import org.example.backend.service.SearchMovieService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/search")
+public class SearchMovieController {
+    private final SearchMovieService searchMovieService;
+    public SearchMovieController(SearchMovieService searchMovieService) {
+        this.searchMovieService = searchMovieService;
+    }
+
+    @GetMapping
+    public List<Movie> getFilteredMovies(String searchText) throws MovieNotFoundException {
+        return searchMovieService.getFilteredMovies(searchText);
+    }
+}
+
