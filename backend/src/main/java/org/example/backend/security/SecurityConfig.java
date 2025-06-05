93a931aec8055793509cb0a5f57753d54667d37a
Yinli Jiang <168180632+jiangyinli0803@users.noreply.github.com>
2025-06-05 19:33:53 +0200

    Update SecurityConfig.java

--- backend/src/main/java/org/example/backend/security/SecurityConfig.java
+++ backend/src/main/java/org/example/backend/security/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .cors(Customizer.withDefaults())
-                .csrf(csrf -> csrf.ignoringRequestMatchers("/logout"))
+                //.csrf(csrf -> csrf.ignoringRequestMatchers("/logout"))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/auth").permitAll()
                         .requestMatchers(HttpMethod.OPTIONS, "/logout").permitAll()
