6a23ce932408bfb2b89284e0fddf9aeef5602a7e
Yinli <jiangyinli0803@gmail.com>
2025-06-05 12:36:11 +0200

    enable CSRF and update AuthController

--- backend/src/main/java/org/example/backend/security/AuthController.java
+++ backend/src/main/java/org/example/backend/security/AuthController.java
@@ -6,14 +6,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
     @GetMapping
     public String getMe(@AuthenticationPrincipal OAuth2User user) {
-        return  user.getAttribute("login").toString();
+        if (user == null) return "Not authenticated";
 
-        //user.getName() return id
+        //GitHub OAuth 返回的用户信息里，有 "login" 这个字段（如用户名）
+        //Google OAuth 返回的是 "name", "email" 等，没有 "login"
+
+        return Optional.ofNullable(user.getAttribute("login"))   // GitHub
+                .or(() -> Optional.ofNullable(user.getAttribute("name")))  // Google
+                .map(Object::toString)
+                .orElse("Unknown user");
     }
 }
--- backend/src/main/java/org/example/backend/security/SecurityConfig.java
+++ backend/src/main/java/org/example/backend/security/SecurityConfig.java
@@ -17,7 +17,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/auth").authenticated()
                         //  .requestMatchers(HttpMethod.GET,"/api/aisearch").hasAuthority("ADMIN")
