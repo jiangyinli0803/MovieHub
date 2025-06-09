4a3c18545800b5cb303bfe6a88d41f412e46dfe7
Yinli <jiangyinli0803@gmail.com>
2025-06-09 21:52:48 +0200

    add User model, User Service und Repository--backend

---
+++ backend/src/main/java/org/example/backend/model/user/User.java
@@ -0,0 +1,31 @@
+package org.example.backend.model.user;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDateTime;
+
+
+@Document("users")
+@Builder
+@Getter
+@Setter
+public class User {
+    @Id
+    private String id;
+
+    // for Form Login
+    private String username;
+    private String email;
+    private String password;
+
+    // OAuth Login
+    private AuthProvider authProvider;
+    private String providerId;
+    private String name;
+    private String avatarUrl;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
---
+++ backend/src/main/java/org/example/backend/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package org.example.backend.repository;
+
+import org.example.backend.model.user.AuthProvider;
+import org.example.backend.model.user.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByAuthProviderAndProviderId(AuthProvider provider, String providerId);
+
+    Optional<User> findByEmail(String email);
+}
